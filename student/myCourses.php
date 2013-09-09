af7c7bb43bbf67f66b8a5fbd431deff1265b4b11
Travis <shankstravis@yahoo.com>
2013-09-09 12:58:38 -0400

    Got mycourses.php to have no errors working on viewsubmission.php

--- student/myCourses.php
+++ student/myCourses.php
@@ -14,18 +14,18 @@ if ($_SESSION['stayLoggedIn'] == true) {
 
 //list courses student is in
 $rosterQueryString = "SELECT * FROM Roster WHERE StudentId=" . $_SESSION['idmember'];
-$rosterQuery = mysqli_query($rosterQueryString);
+$rosterQuery = $mysqli->query($rosterQueryString);
 while ($rosterQueryRow = mysqli_fetch_array($rosterQuery)) {
     //echo classname
     $classnameQueryString = "SELECT * FROM Classes WHERE idClass=" . $rosterQueryRow['ClassId'];
-    $classnameQuery = mysql_query($classnameQueryString);
-    $classnameRow = mysql_fetch_array($classnameQuery);
+    $classnameQuery = $mysqli->query($classnameQueryString);
+    $classnameRow = mysqli_fetch_array($classnameQuery);
     echo "<h1>" . $classnameRow['ClassName'] . "</h1><br />";
 
     //echo assignments
     $assignmentQueryString = "SELECT * FROM Assignment WHERE AssignmentClass=" . $rosterQueryRow['ClassId'];
-    $assignmentQuery = mysql_query($assignmentQueryString);
-    while ($assignmentQueryRow = mysql_fetch_array($assignmentQuery)) {
+    $assignmentQuery = $mysqli->query($assignmentQueryString);
+    while ($assignmentQueryRow = mysqli_fetch_array($assignmentQuery)) {
         echo "<b>Assignment Name:</b> " . $assignmentQueryRow['AssignmentName'] . "<br />";
         echo "<b>Due Date:</b> " . $assignmentQueryRow['AssignmentDueDate'] . "<br />";
         echo "<b>Instructions:</b> " . $assignmentQueryRow['AssignmentInstructions'] . "<br />";
