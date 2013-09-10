2c72e3bf515875b75099105482613afe6198249f
Travis <shankstravis@yahoo.com>
2013-09-10 00:04:40 -0400

    Added more documentation to the results.php and submission.php files

--- student/myCourses.php
+++ student/myCourses.php
@@ -3,37 +3,48 @@
 $title = "My Courses";
 include("../shared_php/header.php");
 
+/*
 //debugging output for staying logged in
 if ($_SESSION['stayLoggedIn'] == true) {
     echo "staying logged in";
 } else {
     echo "not checked";
 }
-
+*/
     echo ("  <br><br> <h3> <u> Current Courses: </u> </h3> <br> ");
 
 //list courses student is in
-$rosterQueryString = "SELECT * FROM Roster WHERE StudentId=" . $_SESSION['idmember'];
-$rosterQuery = $mysqli->query($rosterQueryString);
-while ($rosterQueryRow = mysqli_fetch_array($rosterQuery)) {
-    //echo classname
+$rosterQueryString = "SELECT * FROM Roster WHERE StudentId=" . $_SESSION['idmember'];  //Gathers current students id number
+$rosterQuery = $mysqli->query($rosterQueryString); //Query string
+
+//Outer while loop cycles through each class
+while ($rosterQueryRow = mysqli_fetch_array($rosterQuery)) 
+{
+    //Queries the classes for the current logged in student, then displays class
     $classnameQueryString = "SELECT * FROM Classes WHERE idClass=" . $rosterQueryRow['ClassId'];
     $classnameQuery = $mysqli->query($classnameQueryString);
     $classnameRow = mysqli_fetch_array($classnameQuery);
     echo "<h1>" . $classnameRow['ClassName'] . "</h1><br />";
 
-    //echo assignments
+    //Queries the assignments for the current logged in student
     $assignmentQueryString = "SELECT * FROM Assignment WHERE AssignmentClass=" . $rosterQueryRow['ClassId'];
     $assignmentQuery = $mysqli->query($assignmentQueryString);
-    while ($assignmentQueryRow = mysqli_fetch_array($assignmentQuery)) {
+    
+	//This inner loop will cycle through every assignment for the currently iterated class, then display each assignment under the class
+    while ($assignmentQueryRow = mysqli_fetch_array($assignmentQuery)) 
+    {
         echo "<b>Assignment Name:</b> " . $assignmentQueryRow['AssignmentName'] . "<br />";
         echo "<b>Due Date:</b> " . $assignmentQueryRow['AssignmentDueDate'] . "<br />";
         echo "<b>Instructions:</b> " . $assignmentQueryRow['AssignmentInstructions'] . "<br />";
+        
+        //Will display different depending on what programming 
         if ($assignmentQueryRow['AssignmentType'] == 0) {
             echo "<b>Assignment Type:</b> C++<br />";
         } else {
             echo "<b>Assignment Type:</b> Python<br />";
         }
+		
+		//Displays link to assignment
         echo "<a href='../student/submission.php?idAssignment=" . $assignmentQueryRow['idAssignment'] . "'>Start This Assignment</a><br />";
         echo "<br />";
     }
--- student/submission.php
+++ student/submission.php
@@ -4,26 +4,34 @@ $title = "Submission: Assignment " . $_GET['idAssignment']; //enter title into t
 include("../shared_php/header.php");
 require dirname(__FILE__) . '/../files/KLogger.php';
 $log   = KLogger::instance(dirname(__FILE__) . '/../files/log'.$_SESSION['uniqueID'], KLogger::INFO);
+
 //have to set current assignment ID into a session variable
 $_SESSION['currentAssignmentID'] = $_GET['idAssignment'];
 
 echo ("  <br><br> <h3> <u> Submissions: </u> </h3> <br> ");
 
+//Query selects everything from the assignment table 
 $query = "SELECT * FROM Assignment WHERE idAssignment=" . $_SESSION['currentAssignmentID'];
-$result = mysql_query($query);
-$array = mysql_fetch_array($result);
+$result = $mysqli->query($query);
+$array = mysqli_fetch_array($result);
 
-
+//Displays the query from the assignment table 
 echo "<b>Assignment Name:</b> " . $array['AssignmentName'] . "<br />";
 echo "<b>Instructions:</b> " . $array['AssignmentInstructions'] . "<br />";
-echo "<b>Due Date:</b> " . $array['AssignmentDueDate'] . "<br />";
-echo "<br />";
+echo "<b>Due Date:</b> " . $array['AssignmentDueDate'] . "<br /><br />";
+
+
+//Confused on what they are trying to do here
+
+/*
 $log->LogInfo("in submissions");
+
 //create file to store program from database
-$program = $array['AssignmentCode'];
+$program = $array['AssignmentCode'];  //Puts the entire code in variable
 $filePath = "../files/";
 $fileName = "compiledDatabaseProgram" . $_GET['idAssignment'] . ".py";
 $fullFileName = $filePath . $fileName;
+
 $log->LogInfo($fullFileName);
 fopen($fullFileName, 'w+');
 
@@ -35,12 +43,14 @@ system("python " . $fullFileName . " > " . $filePath . "test.txt");
 
 //get file contents from test.txt and put them into the session variable firstOutput
 $_SESSION['firstOutput'] = file_get_contents($filePath . "test.txt");
+*/
 ?>
 
 <div class="codebox">
     <code>
         <pre>
-            <?php echo $_SESSION['firstOutput']; ?>
+            <?//php echo $_SESSION['firstOutput']; ?>
+            <?php echo $array['AssignmentCode'] ?>
         </pre>
     </code>
 </div>
