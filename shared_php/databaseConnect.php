9ca44a489e7d218bb51c94553c2633363f1d601a
unknown <bboll@kent.edu>
2013-09-23 12:53:20 -0400

    Reverted database connection credentials

--- shared_php/databaseConnect.php
+++ shared_php/databaseConnect.php
@@ -1,18 +1,16 @@
 <?php
-   /*
 	$host = "capstone-s13.cs.kent.edu";   // database server host domain name
 	$uid = "ckerstin";                  // needs to be secured
 	$pass = "capstones13";
 	$dbname = "capstones13";
 	$mysqli = new mysqli($host, $uid, $pass, $dbname);  // connection object
-    */
-    
+   /* 
     $host = "localhost";   // database server host domain name
 	$uid = "root";                  // needs to be secured
 	$pass = "";
 	$dbname = "capstone";
 	$mysqli = new mysqli($host, $uid, $pass, $dbname);  // connection object
-	
+	*/
 	
     /* check connection */
 	if ($mysqli->connect_errno) 
