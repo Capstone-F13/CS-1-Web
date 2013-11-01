9cbe752e58e3e5487827fb5c00fa07d9a94e576c
Travis <shankstravis@yahoo.com>
2013-10-31 21:44:26 -0400

    update

--- js/createAssignment.js
+++ js/createAssignment.js
@@ -1,4 +1,3 @@
-
 function ValidateForm()
 {
     var x = document.forms["create_assignment"]["txtAssignmentName"].value;
@@ -22,6 +21,13 @@ function ValidateForm()
         document.forms["create_assignment"]["txtInstructions"].focus();
         return;
     }
+    var x = document.forms["create_assignment"]["txtCode"].value;
+    if(x == null || x == "")
+    {
+        alert("Please enter Code.");
+        document.forms["create_assignment"]["txtCode"].focus();
+        return;
+    }
     var x = document.forms["create_assignment"]["cmbNoOfAttempts"].value;
     var y = document.forms["create_assignment"]["NoOfSuccessfulAttempts"].value;
     if (x == null || x == "0")
@@ -30,7 +36,7 @@ function ValidateForm()
         document.forms["create_assignment"]["cmbNoOfAttempts"].focus();
         return;
     }
-    if (y == null || x == "0")
+    if (y == null || y == "0")
     {
         alert("Please select No of Successful Attempts.");
         document.forms["create_assignment"]["NoOfSuccessfulAttempts"].focus();
@@ -38,9 +44,9 @@ function ValidateForm()
     }
     if (x < y)
     {
-    	alert("Number of attempts can't be less than successful attempts.");
-    	document.forms["create_assignment"]["NoOfSuccessfulAttempts"].focus();
-    	return;
+            alert("Number of attempts can't be less than successful attempts.");
+            document.forms["create_assignment"]["NoOfSuccessfulAttempts"].focus();
+            return;
     }
     var x = document.forms["create_assignment"]["cmbType"].value;
     if (x == null || x == "-1")
@@ -51,7 +57,4 @@ function ValidateForm()
     
     document.forms["create_assignment"].submit();
 }
-
-
-
 
