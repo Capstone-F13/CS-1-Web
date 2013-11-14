85db11cf82f7ebbbe257478e018bdbfa92ebdd88
Travis <shankstravis@yahoo.com>
2013-11-14 12:29:50 -0500

    update

---
+++ home-page.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    	<title>Home</title>
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"></script>
+    </head>
+
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+			
+			</div>
+			<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+				<div class="user-info row-spacing-8">
+					<span id="login">
+						<button type="button" class="btn btn-default">Login</button>
+					</span>
+					<span id="signup">
+						<button type="button" class="btn btn-default">Signup</button>
+					</span>
+				</div>
+			</div>
+		</div>
+		
+		<div class="row row-spacing-2">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<div class="heading">
+					<h1>Home Page</h1>
+				</div>	
+			</div>	
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<div class="features"> 
+					<h2>Features</h2>
+				</div>
+			</div>
+		</div>
+
+		<div class="row row-spacing-4">
+			<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+				<div class="content">
+					<div class="header">
+						<h3>Standard Assignments</h3>
+					</div>
+				<p>These assignments the students will receive randomly generated code and the student must guess what the output of the 
+				code will be.  This is a great exercise for understanding and following code.</p>
+				</div>
+			</div>
+			<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+				<div class="content">
+					<div class="header">
+						<h3>Unit Tests</h3>
+					</div>
+				<p>Unit Tests are assignments in which an instructor can create a unit test for a particualr assignment and test it against the student's own
+					code to see if the assignment was completed correctly</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
